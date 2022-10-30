4824eabab417c8115fc5fc306cadfdd1797cf9e8
paul_nguyen <paul_nguyen@mckinsey.com>
2022-10-30 20:47:21 +0700

    my-view implement

---
+++ android/app/src/main/java/com/myapp/tracking/CustomView.java
@@ -0,0 +1,16 @@
+package com.myapp.tracking;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+public class CustomView extends View {
+    public CustomView(@NonNull Context context, String title) {
+        super(context);
+    }
+}
